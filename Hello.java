c968eef165fa2ff867a18298bf7aa4dd6f5e6ebd
johnjm2 <69920737+johnjm2@users.noreply.github.com>
2020-08-19 14:35:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/************************
+*Hello
+*************************
+*Joel John
+*8/19/2020
+*CSMC 255-003
+*************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Joel John");
+		System.out.println("Hello");
+		System.out.println("8/19/2020");
+		System.out.println("CSMC 255-003");
+		System.out.println();
+	}
+}
